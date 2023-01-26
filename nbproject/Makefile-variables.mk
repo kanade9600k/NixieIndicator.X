34876e362f6e5f7af372629e6cb2d412d03f91d5
kanade9600k <tomo9600k@gmail.com>
2023-01-26 17:09:11 +0900

    最初のコミット

---
+++ nbproject/Makefile-variables.mk
@@ -0,0 +1,13 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=NixieIndicator.X.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/NixieIndicator.X.production.hex
+CND_PACKAGE_DIR_default=${CND_DISTDIR}/default/package
+CND_PACKAGE_NAME_default=nixieindicator.x.tar
+CND_PACKAGE_PATH_default=${CND_DISTDIR}/default/package/nixieindicator.x.tar
